9a82b8b7556a8463dc4fe067e6f0b17e7db9435f
jinwoo park <swat018@gmail.com>
2020-07-03 08:42:54 +0900

    스프링 데이터 Common: Web - JPA 엔티티 저장하기

--- src/test/java/com/swat018/commonweb/post/PostRepositoryTest.java
+++ src/test/java/com/swat018/commonweb/post/PostRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,11 +19,29 @@ public class PostRepositoryTest {
     @Autowired
     private PostRepository postRepository;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Test
-    public void crud() {
+    public void save() {
         Post post = new Post();
         post.setTitle("jpa");
-        postRepository.save(post);
+        Post savedPost = postRepository.save(post);              // persist
+
+        assertThat(entityManager.contains(post)).isTrue();
+        assertThat(entityManager.contains(savedPost)).isTrue();
+        assertThat(savedPost == post);
+
+        Post postUpdate = new Post();
+        postUpdate.setId(post.getId());
+        postUpdate.setTitle("hibernate");
+        Post updatedPost = postRepository.save(postUpdate);        // merge
+
+        updatedPost.setTitle("whiteship");
+
+        assertThat(entityManager.contains(updatedPost)).isTrue();
+        assertThat(entityManager.contains(postUpdate)).isFalse();
+        assertThat(updatedPost == postUpdate);
 
         List<Post> all = postRepository.findAll();
         assertThat(all.size()).isEqualTo(1);
