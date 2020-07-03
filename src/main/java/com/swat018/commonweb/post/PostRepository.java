38e20f7cf094e5db7523133a6f63d116e80e303a
jinwoo park <swat018@gmail.com>
2020-07-04 08:49:57 +0900

    스프링 데이터  JPA: Named Parameter과 SpEL

--- src/main/java/com/swat018/commonweb/post/PostRepository.java
+++ src/main/java/com/swat018/commonweb/post/PostRepository.java
@@ -3,12 +3,13 @@ package com.swat018.commonweb.post;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByTitleStartsWith(String title);
 
-    @Query("SELECT p FROM Post AS p WHERE p.title = ?1")
-    List<Post> findByTitle(String title, Sort sort);
+    @Query("SELECT p FROM #{#entityName} AS p WHERE p.title = :title")
+    List<Post> findByTitle(@Param("title") String keyword, Sort sort);
 }
