9e920f4a8fc4201e37efbbb10574db7e4b339fbf
jinwoo park <swat018@gmail.com>
2020-07-04 01:27:35 +0900

    스프링 데이터  JPA: 쿼리 메소드 Sort

--- src/main/java/com/swat018/commonweb/post/PostRepository.java
+++ src/main/java/com/swat018/commonweb/post/PostRepository.java
@@ -1,5 +1,6 @@
 package com.swat018.commonweb.post;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,5 +10,5 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByTitleStartsWith(String title);
 
     @Query("SELECT p FROM Post AS p WHERE p.title = ?1")
-    List<Post> findByTitle(String title);
+    List<Post> findByTitle(String title, Sort sort);
 }
