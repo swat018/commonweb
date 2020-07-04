8a92ce99e68be6a4a67962d1de95bbc1fbb4470c
jinwoo park <swat018@gmail.com>
2020-07-04 23:50:10 +0900

    스프링 데이터  JPA: 트랜잭션

--- src/main/java/com/swat018/commonweb/post/CommentRepository.java
+++ src/main/java/com/swat018/commonweb/post/CommentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +15,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpec
     @EntityGraph(attributePaths = "post")
     Optional<Comment> getById(Long id);
 
+    @Transactional(readOnly = true)
     <T> List<T> findByPost_Id(Long id, Class<T> type);
 
 }
