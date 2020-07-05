c4e449327fba20627ea5dfd5f8832ed763b7ab90
jinwoo park <swat018@gmail.com>
2020-07-05 11:36:42 +0900

    JPA - 엔티티 타입 (Enum)

--- src/main/java/com/swat018/commonweb/post/Comment.java
+++ src/main/java/com/swat018/commonweb/post/Comment.java
@@ -20,6 +20,9 @@ public class Comment {
 
     private String comment;
 
+    @Enumerated(value = EnumType.STRING)
+    private CommentStatus commentStatus;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private Post post;
 
---
+++ src/main/java/com/swat018/commonweb/post/CommentStatus.java
@@ -0,0 +1,6 @@
+package com.swat018.commonweb.post;
+
+public enum CommentStatus {
+
+    DRAFT, PUBLISHED, DELETED
+}
