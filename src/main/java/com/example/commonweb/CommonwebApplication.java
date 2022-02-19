0804143423d3e6f36c8198a5581e2027422e46c1
jinwoo park <swat018@gmail.com>
2022-02-19 16:44:36 +0900

    Initial commit

---
+++ src/main/java/com/example/commonweb/CommonwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.commonweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonwebApplication.class, args);
+	}
+
+}
