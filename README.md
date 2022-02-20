23e1b273a7e2a341d689dc04a811eae9d2734c30
jinwoo park <swat018@gmail.com>
2022-02-20 16:01:39 +0900

    feat : 스프링 데이터 Common - Web : DomainClassConverter / Pageable과 Sort 매개변수

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # 백기선 - 스프링 데이터 JPA
 - 2부. 스프링 데이터 JPA 활용
   -  
+  - 스프링 데이터 Common: Web - DomainClassConverter
   - 스프링 데이터 Common: Web - Pageable과 Sort 매개변수
---
+++ src/test/java/com/example/commonweb/Post/PostControllerTest.java
@@ -0,0 +1,60 @@
+package com.example.commonweb.Post;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class PostControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    public void getPost() throws Exception {
+        Post post = new Post();
+        post.setTitle("jpa");
+        postRepository.save(post);
+
+        mockMvc.perform(get("/posts/" + post.getId()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("jpa"));
+    }
+
+    @Test
+    public void getPosts() throws Exception {
+        Post post = new Post();
+        post.setTitle("jpa");
+        postRepository.save(post);
+
+        mockMvc.perform(get("/posts/")
+                        .param("page", "0")
+                        .param("size", "10")
+                        .param("sort", "created,desc")
+                        .param("sort", "title"))
+                .andDo(print())
+                .andExpect(status().isOk());
+//                .andExpect(jsonPath("$.content[0].title"), is("jpa"));
+    }
+}
