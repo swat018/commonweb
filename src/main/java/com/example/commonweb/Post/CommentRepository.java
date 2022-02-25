b5fbbaeaf600f2f5ce336b793281e780ef25b059
jinwoo park <swat018@gmail.com>
2022-02-25 20:53:15 +0900

    feat : 스프링 데이터 JPA - Query by Example

--- src/main/java/com/example/commonweb/Post/CommentRepository.java
+++ src/main/java/com/example/commonweb/Post/CommentRepository.java
@@ -3,10 +3,11 @@ package com.example.commonweb.Post;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 
 import java.util.List;
 
-public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpecificationExecutor<Comment> {
+public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpecificationExecutor<Comment>, QueryByExampleExecutor<Comment> {
 
 //    @EntityGraph(value = "Comment.post")
     @EntityGraph(attributePaths = "post")
